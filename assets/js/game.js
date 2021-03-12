801b3b29c5f2e45f37d9f8ed9c5c23b04f6d2130
Coco Puff <fahrija_wyroski@live.com>
2021-03-11 18:39:50 -0600

    Handle blank, null and mixed case user input

--- assets/js/game.js
+++ assets/js/game.js
@@ -161,10 +161,22 @@ var shop = function () {
     }
 };
 
+// function to set name
+var getPlayerName = function () {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+
+};
 
 // assign variable to user's name
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
